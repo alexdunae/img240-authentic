5c5e3d187fe7966b1ba31c38c52feb3f3e736e46
Alex Dunae <alex@dunae.ca>
2015-03-18 15:16:11 -0700

    Loginable

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: session_params[:email])
+    if user && user.authenticate(session_params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: 'Hi, friend!'
+    else
+      redirect_to root_path, notice: 'No way!'
+    end
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
+end
